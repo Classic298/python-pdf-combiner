2fdca94111ad89881ecc68a9f29aeb1b780a1e56
Classic <ja.gruber@outlook.com>
2020-10-23 20:26:39 +0200

    Added a comment

--- merge.py
+++ merge.py
@@ -1,5 +1,5 @@
 from PyPDF2 import PdfFileMerger
-
+# "py -m pip install PyPDF2" you need the python module "PyPDF2" for this to work
 pdfs = ['1.pdf', '2.pdf', '3.pdf']
 
 merger = PdfFileMerger(strict=False)
